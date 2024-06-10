0c5a49dbed834e5bcdaaccb8331eb4a444ca9bca
chinfanster <chinfanster@gmail.com>
2024-06-10 15:59:11 -0700

    testing

--- src/App.jsx
+++ src/App.jsx
@@ -29,12 +29,12 @@ function App() {
       <StayConnected /> */}
       <Routes>
         <Route path="/" element={<HomePage />} />
-        <Route path="#/vets" element={<ForVetPage />} />
-        <Route path="#/clients" element={<HomePage />} />
-        <Route path="#/register" element={<RegisterPage />} />
-        <Route path="#/redirect" element={<RedirectPage />} />
-        <Route path="#/privacy-policy" element={<PrivacyPolicyPage />} />
-        <Route path="#/SMSTerms" element={<SMSTermsPage />} />
+        <Route path="/vets" element={<ForVetPage />} />
+        <Route path="/clients" element={<HomePage />} />
+        <Route path="/register" element={<RegisterPage />} />
+        <Route path="/redirect" element={<RedirectPage />} />
+        <Route path="/privacy-policy" element={<PrivacyPolicyPage />} />
+        <Route path="/SMSTerms" element={<SMSTermsPage />} />
       </Routes>
       <GetStartedCallout />
       <StayConnected />
