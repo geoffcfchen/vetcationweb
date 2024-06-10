99af40edd74877cc5e5cd846d597367498a1866b
chinfanster <chinfanster@gmail.com>
2024-06-10 16:08:40 -0700

    fix navigation

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -126,8 +126,8 @@ function Header() {
       </LogoContainer>
 
       <Nav>
-        <MenuLink href="/clients">for Pet Owners</MenuLink>
-        <MenuLink href="/vets">for Vets</MenuLink>
+        <MenuLink href="#/clients">for Pet Owners</MenuLink>
+        <MenuLink href="#/vets">for Vets</MenuLink>
         {/* <MenuLink href="/techs">for Techs</MenuLink> */}
         {/* <MenuLink href="/hospitals">for Hospitals</MenuLink> */}
         {/* <MenuLink href="/students">for Students</MenuLink> */}
